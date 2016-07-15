4677ad7d4fa63a05e1ea5aa5f26f01a591656320
Tom Yang <xy2286@columbia.edu>
2016-07-15 19:04:58 -0400

    reverse a string

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -1,9 +1,16 @@
-# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# Complete the method called reverse_a_string that accepts a string as a parameter and
 # returns the reverse. The one caveat: Don't use the reverse method that already
 # comes with Ruby!
 
 def reverse_a_string(string)
-
+  num = 0
+  while num < (string.length / 2)
+    intermed = string[string.length - num - 1]
+    string[string.length - num - 1] = string[num]
+    string[num] = intermed
+    num = num + 1
+  end
+  return string
 end
 
 # Driver code - don't touch anything below this line.
